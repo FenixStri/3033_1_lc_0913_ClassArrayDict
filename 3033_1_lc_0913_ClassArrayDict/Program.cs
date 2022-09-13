9ff43c46e01c9f58bbe99369f3d993eaa496737f
Joe Jennings <lane@lanestrickland.com>
2022-09-13 16:14:22 -0500

    Add project files.

---
+++ 3033_1_lc_0913_ClassArrayDict/Program.cs
@@ -0,0 +1,45 @@
+﻿// Fenix Strickland
+// MIS 3033 001 class
+
+
+Person p1;
+p1 = new Person();
+Console.WriteLine(p1.Name);
+//p1.Name = "David";
+
+Student stu;
+stu = new Student();
+
+Console.WriteLine(p1.);
+
+Console.ReadLine();
+
+public class Person
+{
+
+    private string name = "Fenix Strickland";
+
+    public int age { get; set; } = 20;
+
+    public double weight { get; set; } = 120;
+
+    //public string Name { get; set; }
+    public string Name { get 
+        {
+            return name.Substring(0,2)+"***";
+        }
+    
+    }
+
+}
+
+public class Student:Person
+{
+    public string stuID { get; set; } = "defaultOUID";
+
+   public void PrintInfor()
+    {
+        Console.WriteLine(this.weight);
+        Console.WriteLine(this.Name);
+    }
+}
